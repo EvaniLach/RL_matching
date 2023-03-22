28d5c04907cdafc19ed914ea25870e403078867b
Merel <merel@wemel.nl>
2023-03-22 11:30:38 +0100

    updated main

--- main.py
+++ main.py
@@ -22,29 +22,15 @@ def main():
     for path in paths:
         SETTINGS.check_dir_existence(SETTINGS.home_dir + path)
 
+    print("CREATING ENVIRONMENT")
+    env = MatchingEnv(SETTINGS, PARAMS)
+    print("CREATING DQN")
+    dqn = DQN(SETTINGS, env)
 
-    # Sample demand for each day in the simulation, and write to a csv file.
-    if SETTINGS.mode == "demand":
-        for htype in SETTINGS.n_hospitals.keys():
-            for _ in range(SETTINGS.n_hospitals[htype]):
-                generate_demand(SETTINGS, PARAMS, htype, SETTINGS.avg_daily_demand[htype])
+    print(f"alpha: {SETTINGS.alpha}, gamma: {SETTINGS.gamma}, batch size: {SETTINGS.batch_size}.")
 
-    # Sample RBC units to be used as supply in the simulation, and write to csv file.
-    elif SETTINGS.mode == "supply":
-        generate_supply(SETTINGS, PARAMS)
-
-    # Run the simulation, using either linear programming or reinforcement learning to determine the matching strategy.
-    elif SETTINGS.mode == "train":
-
-        print("CREATING ENVIRONMENT")
-        env = MatchingEnv(SETTINGS, PARAMS)
-        print("CREATING DQN")
-        dqn = DQN(SETTINGS, env)
-
-        print(f"alpha: {SETTINGS.alpha}, gamma: {SETTINGS.gamma}, batch size: {SETTINGS.batch_size}.")
-
-        # Train the agent
-        dqn.train(SETTINGS, PARAMS)
+    # Train the agent
+    dqn.train(SETTINGS, PARAMS)
 
 
 
